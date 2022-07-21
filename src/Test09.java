e819e7acab705cd1c5f077b85fafb2339d8f4f84
fwangjuwon <fwangjuwon@naver.com>
2022-07-21 21:26:51 +0900

    정처기실기 프로그래밍언어 -2

---
+++ src/Test09.java
@@ -0,0 +1,9 @@
+public class Test09 {
+    public static void main(String[] args) {
+        String str;
+        str = "Power overwhelming!";
+        System.out.printf("%8.4s\n", str);
+    }
+}
+
+// %8.4는 8자리를 확보한 후, 앞에서부터 4글자까지 들고오는 것
